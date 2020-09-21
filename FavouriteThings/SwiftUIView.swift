50c71acf718a76212ebde516fb74a31eca7b31b3
Luke Burri <LBUrri@lcs.on.ca>
2020-09-21 11:17:00 -0400

    error first commit

---
+++ FavouriteThings/SwiftUIView.swift
@@ -0,0 +1,30 @@
+////
+////  SwiftUIView.swift
+////  FavouriteThings
+////
+////  Created by Burri, Luke on 2020-09-15.
+////  Copyright © 2020 Burri, Luke. All rights reserved.
+////
+//
+//import SwiftUI
+//
+//struct SwiftUIView: View {
+//    
+////the "body" property in a structure
+////that conformes to the View protocol must return
+////ONE and only ONe view at the toop oof the hierarchy
+//    var body: some View {
+//        
+//        //one view at the top
+//        ScrollView {}
+//        VStack
+//        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+//            .padding(.horizontal, 15.0)
+//    }
+//}
+//
+//struct SwiftUIView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUIView()
+//    }
+//}
